9bd18ab7146edad71632760f20924e781ffb22fb
duyanh14 <duyanhnguyen2001@gmail.com>
2022-05-31 11:54:00 +0700

    Update

--- Assets/Scripts/Canvas.cs
+++ Assets/Scripts/Canvas.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Canvas : MonoBehaviour
 {
+    public EarningPopup popup;
+    
     public Button openPopupBtn;
     private Animator openPopupBtnAnimator;
-
+    
+    public Button closePopupBtn;
+    
     void Start()
     {
         openPopupBtnAnimator = openPopupBtn.GetComponent<Animator>();
         openPopupBtn.onClick.AddListener(openPopup);
+        closePopupBtn.onClick.AddListener(closePopup);
     }
 
     private void openPopup()
     {
-        openPopupBtnAnimator.SetTrigger("Disabled");
+        openPopupBtnAnimator.Play("Disabled");
+        popup.gameObject.SetActive(true);
     }
 
     public void closePopup()
     {
+        popup.gameObject.SetActive(false);
         openPopupBtnAnimator.SetTrigger("Normal");
-    }
-
-    void Update()
-    {
+        openPopupBtnAnimator.enabled = true;
     }
 }
